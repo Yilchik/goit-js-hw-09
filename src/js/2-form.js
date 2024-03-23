577f0deba1a0480c097f1054df73714b03e98cdc
Yilchik <yulyayulia2@gmail.com>
2024-03-23 15:35:21 +0100

    form

---
+++ src/js/2-form.js
@@ -0,0 +1,33 @@
+const registerForm = document.querySelector('form');
+const localStorageKey = 'feedback-form-state';
+
+registerForm.addEventListener('input', evt => {
+  const saveInfo = {
+    email: registerForm.email.value,
+    message: registerForm.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(saveInfo));
+});
+
+registerForm.addEventListener('submit', handlerSubmit);
+
+const savedData = localStorage.getItem(localStorageKey);
+
+if (savedData) {
+  const { email, message } = JSON.parse(savedData);
+  registerForm.email.value = email;
+  registerForm.message.value = message;
+}
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  const data = {
+    email: event.target.elements.email.value,
+    message: event.target.elements.message.value,
+  };
+
+  console.log(data);
+  localStorage.removeItem(localStorageKey);
+  registerForm.reset();
+}
