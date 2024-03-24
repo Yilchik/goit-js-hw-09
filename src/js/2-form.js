51a9ac98d626f87c9114f5c7bb92d30b11e90037
Yilchik <yulyayulia2@gmail.com>
2024-03-24 13:19:38 +0100

    hw9-end

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,6 @@
 const registerForm = document.querySelector('form');
 const localStorageKey = 'feedback-form-state';
 
-registerForm.addEventListener('input', () => {
-  const saveInfo = {
-    email: registerForm.email.value,
-    message: registerForm.message.value,
-  };
-  localStorage.setItem(localStorageKey, JSON.stringify(saveInfo));
-});
-
-registerForm.addEventListener('submit', handlerSubmit);
-
 const savedData = localStorage.getItem(localStorageKey);
 
 if (savedData) {
@@ -19,12 +9,22 @@ if (savedData) {
   registerForm.message.value = message;
 }
 
+registerForm.addEventListener('input', () => {
+  const saveInfo = {
+    email: registerForm.email.value.trim(),
+    message: registerForm.message.value.trim(),
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(saveInfo));
+});
+
+registerForm.addEventListener('submit', handlerSubmit);
+
 function handlerSubmit(event) {
   event.preventDefault();
 
   const data = {
-    email: event.target.elements.email.value,
-    message: event.target.elements.message.value,
+    email: event.target.elements.email.value.trim(),
+    message: event.target.elements.message.value.trim(),
   };
 
   console.log(data);
